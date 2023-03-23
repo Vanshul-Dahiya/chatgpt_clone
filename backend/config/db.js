96abbf925cda2c61959aee440c5977d8232cced9
Vanshul <vanshuldahiya2002@gmail.com>
2023-03-23 09:20:39 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected to MongoDB : ${mongoose.connection.host}`.bgGreen.white
+    );
+  } catch (error) {
+    console.log(`MongoDB error : ${error}`.bgRed.white);
+  }
+};
+module.exports = connectDB;
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// mongo connection
+connectDB();
+
+// rest object
+const app = express();
+
+// middleWares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 8080;
+// listen
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`.bgCyan.black);
+});
